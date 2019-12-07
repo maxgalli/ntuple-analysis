c5593a81c8f7985ced9dba4ba46bd38a580dd73b
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-12-07 12:29:14 +0100

    [Config] Added DrellYan weights selection
    Is adding "_ntLO" the most convenient way to distinguish?
    Cuts not added due to channel dependency.

---
+++ ntuple_config/legacy_smhtt_2017/process_selection.py
@@ -0,0 +1,31 @@
+from ntuple_processor.utils import Selection
+
+DY_process_base_weights = [
+    ("puweight", "puweight"),
+    ("idWeight_1*idWeight_2","idweight"),
+    ("isoWeight_1*isoWeight_2","isoweight"),
+    ("trackWeight_1*trackWeight_2","trackweight"),
+    ("eleTauFakeRateWeight*muTauFakeRateWeight", "leptonTauFakeRateWeight"),
+    ("zPtReweightWeight", "zPtReweightWeight"),
+    ("prefiringweight", "prefireWeight")
+    ]
+DY_process_weights = DY_process_base_weights
+DY_process_weights_ntLO = DY_process_base_weights
+DY_process_weights.append((
+        "((genbosonmass >= 50.0)*6.3654e-05*((npartons == 0 || npartons >= 5)*1.0 + (npartons == 1)*0.1743 \
+                + (npartons == 2)*0.3556 \
+                + (npartons == 3)*0.2273 \
+                + (npartons == 4)*0.2104) \
+                + (genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)",
+         "z_stitching_weight"
+         ))
+DY_process_weights_ntLO.append((
+        "((genbosonmass >= 50.0)*2.9688e-05 \
+                + (genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)",
+        "z_stitching_weight"
+        ))
+
+DY_process_selection = Selection(name = "DrellYan",
+                                 weights = DY_process_weights)
+DY_ntLO_process_selection = Selection(name = "DrellYan_ntLO",
+                                      weights = DY_process_weights_ntLO)
