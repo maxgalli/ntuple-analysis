40338fec43b4c4d5f09b4923a2501d5914f7dd6a
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-12-15 10:38:44 +0100

    [Config] Commented out invalid cut

--- ntuple_config/legacy_smhtt_2017/channel_selection.py
+++ ntuple_config/legacy_smhtt_2017/channel_selection.py
@@ -11,5 +11,5 @@ mt_channel = Selection(name = "mt",
                            ("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
                            ("iso_1<0.15", "muon_iso"),
                            ("q_1*q_2<0", "os"),
-                           ("trg_singlemuon==1", "trg_singlemuon")
+                           #("trg_singlemuon==1", "trg_singlemuon")
                            ])
