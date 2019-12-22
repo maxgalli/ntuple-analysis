54d94597228d7061a7d2ca4507b2aec8e647d4bd
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-12-22 10:51:08 +0100

    [misc] ntLO -> nlo

--- ntuple_config/legacy_smhtt_2017/process_selection.py
+++ ntuple_config/legacy_smhtt_2017/process_selection.py
@@ -10,7 +10,7 @@ DY_process_base_weights = [
     ("prefiringweight", "prefireWeight")
     ]
 DY_process_weights = DY_process_base_weights
-DY_process_weights_ntLO = DY_process_base_weights
+DY_process_weights_nlo = DY_process_base_weights
 DY_process_weights.append((
         "((genbosonmass >= 50.0)*6.3654e-05*((npartons == 0 || npartons >= 5)*1.0 + (npartons == 1)*0.1743 \
                 + (npartons == 2)*0.3556 \
@@ -19,7 +19,7 @@ DY_process_weights.append((
                 + (genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)",
          "z_stitching_weight"
          ))
-DY_process_weights_ntLO.append((
+DY_process_weights_nlo.append((
         "((genbosonmass >= 50.0)*2.9688e-05 \
                 + (genbosonmass < 50.0)*numberGeneratedEventsWeight*crossSectionPerEventWeight)",
         "z_stitching_weight"
@@ -27,5 +27,5 @@ DY_process_weights_ntLO.append((
 
 DY_process_selection = Selection(name = "DrellYan",
                                  weights = DY_process_weights)
-DY_ntLO_process_selection = Selection(name = "DrellYan_ntLO",
-                                      weights = DY_process_weights_ntLO)
+DY_nlo_process_selection = Selection(name = "DrellYan_nlo",
+                                      weights = DY_process_weights_nlo)
