dcfb7704d71144a49d4f219f5217553d317d8d55
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2019-12-27 19:00:21 +0100

    [Optimization] Defined __repr__ method for class Node

--- ntuple_processor/utils/_optimization.py
+++ ntuple_processor/utils/_optimization.py
@@ -23,6 +23,9 @@ class Node:
             bar ])
         return layout
 
+    def __repr__(self):
+        return self.name
+
     def __eq__(self, other):
         logger.debug('__eq__ compares: names ({} with {}), kinds ({} and {}) and AFU blocks ({} and {})'.format(
                 self.name, other.name,
