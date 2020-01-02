1b5e41641e6249ab01bcf906f819e43c22a1e50f
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2020-01-02 20:38:22 +0100

    [Run] Added class RDataFrameEssentials

--- ntuple_processor/utils/_run.py
+++ ntuple_processor/utils/_run.py
@@ -1,2 +1,10 @@
 import logging
 logger = logging.getLogger(__name__)
+
+class RDataFrameEssentials:
+    def __init__(self,
+            rdataframes,
+            ttrees, tfiles):
+        self.rdataframes = rdataframes
+        self.ttrees = ttrees
+        self.tfiles = tfiles
