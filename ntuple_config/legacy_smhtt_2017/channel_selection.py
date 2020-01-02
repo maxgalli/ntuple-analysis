cc64ba7030616bc2ada22642e9a46221787f692b
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2020-01-02 14:41:41 +0100

    [Config] Commented out 'mt' cut that seems to take everything away for a DY dataset

--- ntuple_config/legacy_smhtt_2017/channel_selection.py
+++ ntuple_config/legacy_smhtt_2017/channel_selection.py
@@ -8,7 +8,7 @@ mt_channel = Selection(name = "mt",
                            ("againstMuonTight3_2>0.5", "againstMuonDiscriminator"),
                            ("againstElectronVLooseMVA6_2>0.5",
                                "againstElectronDiscriminator"),
-                           ("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
+                           #("byTightIsolationMVArun2v1DBoldDMwLT_2>0.5", "tau_iso"),
                            ("iso_1<0.15", "muon_iso"),
                            ("q_1*q_2<0", "os"),
                            #("trg_singlemuon==1", "trg_singlemuon")
