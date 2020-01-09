3fe1bd26d4c80ffbea43333384aaa39eceb64dcc
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2020-01-09 14:25:36 +0100

    [Config] ss and os instead of same_sign and opposite_sign

--- ntuple_config/legacy_smhtt_2017/sign_selection.py
+++ ntuple_config/legacy_smhtt_2017/sign_selection.py
@@ -1,7 +1,7 @@
 from ntuple_processor.utils import Selection
 
-same_sign = Selection(name = "same_sign",
+same_sign = Selection(name = "ss",
     cuts = [("q_1*q_2>0", "ss")])
 
-opposite_sign = Selection(name = "opposite_sign",
+opposite_sign = Selection(name = "os",
     cuts = [("q_1*q_2<0", "os")])
