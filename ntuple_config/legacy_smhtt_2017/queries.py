c20a5972ead03f775dd3d6e7fefc5d9b09cff487
Massimiliano Galli <massimiliano.galli.95@gmail.com>
2020-01-13 15:23:15 +0100

    [Config] Updated DY queries

--- ntuple_config/legacy_smhtt_2017/queries.py
+++ ntuple_config/legacy_smhtt_2017/queries.py
@@ -17,14 +17,14 @@ queryM10 = {
     "generator": "madgraph\-pythia8",
     "extension": "ext1",
     "version": "v2"
-    }
+}
 queryM50_inclusive_2_3jet = {
     "process": "DY(|2|3)JetsToLL_M50",
     "data": False,
     "campaign": "RunIIFall17MiniAODv2",
     "generator": "madgraph\-pythia8",
-    "version": "v1"
-    }
+    "version": "(v1|v2)"
+}
 queryM50_1jet_v1 = {
     "process": "DY1JetsToLL_M50",
     "data": False,
@@ -32,7 +32,7 @@ queryM50_1jet_v1 = {
     "generator": "madgraph\-pythia8",
     "extension": "^$",
     "version": "v1"
-    }
+}
 queryM50_1jet_ext1_v2 = {
     "process": "DY1JetsToLL_M50",
     "data": False,
@@ -40,26 +40,26 @@ queryM50_1jet_ext1_v2 = {
     "generator": "madgraph\-pythia8",
     "extension": "ext1",
     "version": "v2"
-    }
+}
 queryM50_4jet = {
     "process": "DY4JetsToLL_M50",
     "data": False,
     "campaign": "RunIIFall17MiniAODv2",
     "generator": "madgraph\-pythia8",
     "version": "v2"
-    }
+}
 queryEWKZ = {
     "process": "^EWKZ",
     "data": False,
     "campaign": "RunIIFall17MiniAODv2",
     "generator": "madgraph\-pythia8",
-    }
+}
 queryM50NLO_inc = {
     "process": "DYJetsToLL_M50",
     "data": False,
     "campaign": "RunIIFall17MiniAODv2",
     "generator": "amcatnlo\-pythia8",
-    }
+}
 DY_query_nlo = [
     queryM50NLO_inc, queryEWKZ
     ]
